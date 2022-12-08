080237f29841e32a1b54a5d56c003b8d08b4daf2
tau3 <smr.oznob@gmail.com>
2022-12-08 13:18:13 +0300

    initial solution to day 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
+use anyhow::Result;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+pub fn day1(input: &dyn AsRef<[&str]>) -> Result<(u32, u32)> {
+    let mut x = Vec::new();
+    let mut current = Vec::new();
+    for line in input.as_ref() {
+        if line.is_empty() {
+            x.push(current);
+            current = Vec::new();
+            continue;
+        }
+        let calories: u32 = line.parse()?;
+        current.push(calories);
+    }
+    return Ok((0, 0));
 }
 
 #[cfg(test)]
@@ -9,7 +26,14 @@ mod tests {
     #[test]
     fn it_works() {
         let result = add(2, 2);
-	let x =5;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_day1() {
+        let actual = day1(&[
+            "1000", "2000", "3000", "", "4000", "5000", "6000", "", "7000", "8000", "9000", "10000",
+        ]);
+        assert_eq!(actual.unwrap(), (4, 24000));
     }
 }

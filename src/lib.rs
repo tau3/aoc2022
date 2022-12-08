9d92da95458d1d065c4a514f5992fa1d359d412e
tau3 <smr.oznob@gmail.com>
2022-12-08 16:37:58 +0300

    initial solution for day 2

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,68 @@ pub fn day1(input: &dyn AsRef<[&str]>) -> Result<(usize, u32)> {
         .map(|(index, calories)| (index + 1, calories.to_owned()));
 }
 
+pub fn day2(input: Vec<&str>) -> u32 {
+    let mut total = 0;
+    for line in input {
+        let him = line.chars().nth(0).unwrap();
+        let you = line.chars().nth(2).unwrap();
+        total += score(him, you);
+    }
+    return total;
+}
+
+fn score_for_pick(pick: char) -> u32 {
+    match pick {
+        'R' => 1,
+        'P' => 2,
+        'S' => 3,
+        _ => panic!("wrong pick") // TODO fmt + result
+    }
+}
+
+fn score(mut him: char, mut you: char) -> u32 {
+    him = translate(him);
+    you = translate(you);
+
+    let mut result = score_for_pick(you);
+
+    if him == you {
+        return 3 + result;
+    }
+
+    match you {
+        'R' => {
+            if him == 'S' {
+                result += 6;
+            }
+        }
+        'P' => {
+            if him == 'R' {
+                result += 6;
+            }
+        }
+        'S' => {
+            if him == 'P' {
+                result += 6;
+            }
+        }
+        _ => {
+            panic!("error input!"); // TODO format + result
+        }
+    }
+    println!("score={}", result);
+    result
+}
+
+fn translate(choise: char) -> char {
+    match choise {
+        'A' | 'X' => 'R',
+        'B' | 'Y' => 'P',
+        'C' | 'Z' => 'S',
+        _ => panic!("error input!"), // TODO fomrat + result
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -34,5 +96,11 @@ mod tests {
         ]);
 
         assert_eq!(actual.unwrap(), (4, 24000));
+    }
+
+    #[test]
+    fn test_day2() {
+        let actual = day2(vec!["A Y", "B X", "C Z"]);
+        assert_eq!(actual, 15);
     }
 }

a23e149980c92d6fc180196d3a3a30c8c47d8eb2
tau3 <smr.oznob@gmail.com>
2022-12-10 01:39:13 +0300

    refactor day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,51 +1,64 @@
-fn score_for_pick(pick: char) -> u32 {
-    match pick {
-        'R' => 1,
-        'P' => 2,
-        'S' => 3,
-        _ => panic!("wrong pick: {}", pick),
+#[derive(PartialEq)]
+enum Pick {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Pick {
+    pub fn score_for_pick(&self) -> u32 {
+        match self {
+            Pick::Rock => 1,
+            Pick::Paper => 2,
+            Pick::Scissors => 3,
+        }
+    }
+
+    fn from_his_pick(pick: char) -> Self {
+        match pick {
+            'A' => Pick::Rock,
+            'B' => Pick::Paper,
+            'C' => Pick::Scissors,
+            _ => panic!("from_his_pack: wrong pick: {}", pick),
+        }
+    }
+
+    fn from_your_pick(pick: char) -> Self {
+        match pick {
+            'X' => Pick::Rock,
+            'Y' => Pick::Paper,
+            'Z' => Pick::Scissors,
+            _ => panic!("from_your_pick: wrong picK: {}", pick),
+        }
     }
 }
 
-fn score(mut him: char, mut you: char) -> u32 {
-    him = translate(him);
-    you = translate(you);
-
-    let mut result = score_for_pick(you);
-
+fn score(him: char, you: char) -> u32 {
+    let him = Pick::from_his_pick(him);
+    let you = Pick::from_your_pick(you);
+    let result = you.score_for_pick();
     if him == you {
         return 3 + result;
     }
 
-    match you {
-        'R' => {
-            if him == 'S' {
-                result += 6;
-            }
-        }
-        'P' => {
-            if him == 'R' {
-                result += 6;
-            }
-        }
-        'S' => {
-            if him == 'P' {
-                result += 6;
-            }
-        }
-        _ => {
-            panic!("error input!"); // TODO format + result
-        }
-    }
-    result
+    result + score_for_round(him, you)
 }
 
-fn translate(choise: char) -> char {
-    match choise {
+fn score_for_round(him: Pick, you: Pick) -> u32 {
+    match (you, him) {
+        (Pick::Rock, Pick::Scissors)
+        | (Pick::Paper, Pick::Rock)
+        | (Pick::Scissors, Pick::Paper) => 6,
+        _ => 0,
+    }
+}
+
+fn translate(choice: char) -> char {
+    match choice {
         'A' | 'X' => 'R',
         'B' | 'Y' => 'P',
         'C' | 'Z' => 'S',
-        _ => panic!("error input: {}", choise),
+        _ => panic!("error input: {}", choice),
     }
 }
 
@@ -60,13 +73,50 @@ pub fn day2(input: Vec<&str>) -> u32 {
     total
 }
 
+pub fn part2(mut him: char, outcome: char) -> u32 {
+    him = translate(him);
+    let you = translate_outcome(him, outcome);
+    score(him, you)
+}
+
+fn translate_outcome(_him: char, _outcome: char) -> char {
+    //x -lose, y - dray
+    // match (him, outcome) {
+    // 	('A', 'X') => {''}
+    // 	('A', 'Y') => {''}
+    // 	('A', 'Z') => {}
+    // 	('B', 'X') => {}
+    // 	('B', 'Y') => {}
+    // 	('B', 'Z') => {}
+    // 	('C', 'X') => {}
+    // 	('C', 'Y') => {}
+    // 	('C', 'Z') => {}
+    // 	_ => panic!("Foo")
+    // }
+    'A'
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
+    use std::path::PathBuf;
 
     #[test]
     fn test_day2() {
         let actual = day2(vec!["A Y", "B X", "C Z"]);
         assert_eq!(actual, 15);
+    }
+
+    #[test]
+    fn test_with_real_data() {
+        let mut dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        dir.push("resources/test/day2");
+
+        let contents = fs::read_to_string(dir).unwrap();
+        let lines: Vec<&str> = contents.lines().collect();
+
+        let actual = day2(lines);
+        assert_eq!(actual, 9177, "actual is {}", actual);
     }
 }

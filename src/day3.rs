875952df548e2bcf8e6a2528554d77d76187582f
tau3 <smr.oznob@gmail.com>
2022-12-11 22:14:37 +0300

    try solve day3 part2

--- src/day3.rs
+++ src/day3.rs
@@ -1,10 +1,11 @@
 use std::collections::HashSet;
 
-pub fn solve(input: &[&str]) -> usize {
+pub fn solve<S: AsRef<str>>(input: &[S]) -> usize {
     input
         .iter()
-        .map(|x| find_item(x))
-        .map(|x| item_to_priority(x))
+        .map(|items| items.as_ref())
+        .map(find_item)
+        .map(item_to_priority)
         .sum()
 }
 
@@ -20,8 +21,8 @@ fn find_item(line: &str) -> char {
     let mut intersection = left.intersection(&right);
     let result = intersection
         .next()
-        .expect(&format!("no intersection in {}", line));
-    result.clone()
+        .unwrap_or_else(|| panic!("no intersection in {}", line));
+    *result
 }
 
 const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -29,12 +30,41 @@ const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 fn item_to_priority(item: char) -> usize {
     1 + ALPHABET
         .find(item)
-        .expect(&format!("can't find char {}", item))
+        .unwrap_or_else(|| panic!("can't find char {}", item))
+}
+
+pub fn solve_part2<S: AsRef<str>>(input: &Vec<S>) -> usize {
+    let mut result = 0;
+    for i in 0..input.len() / 3 {
+        let badge = badge(&[
+            input[i].as_ref(),
+            input[i + 1].as_ref(),
+            input[i + 2].as_ref(),
+        ]);
+        let score = item_to_priority(badge);
+        result += score;
+    }
+    result
+}
+
+fn badge(group: &[&str; 3]) -> char {
+    let mut result: HashSet<char> = group[0].chars().collect();
+    for item in group.iter().skip(1) {
+        let distint: HashSet<char> = item.chars().collect();
+        let intersection: HashSet<&char> = result.intersection(&distint).collect();
+        result = intersection.iter().copied().copied().collect();
+    }
+
+    result
+        .into_iter()
+        .next()
+        .unwrap_or_else(|| panic!("expect to have badge in group {:?}", group))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::*;
 
     #[test]
     fn test_find_item() {
@@ -50,8 +80,8 @@ mod tests {
 
     #[test]
     fn test_solve() {
-        let input = [
-	    "vJrwpWtwJgWrhcsFMMfFFhFp",
+        let input = vec![
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
             "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
             "PmmdzqPrVvPwwTWBwg",
             "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
@@ -59,5 +89,29 @@ mod tests {
             "CrZsJsPPZsGzwwsLwLmpwMDw",
         ];
         assert_eq!(solve(&input), 157);
+    }
+
+    #[test]
+    fn test_solve_with_real_data() {
+        let input = util::read_real_data("day3");
+        let actual = solve(&input);
+        assert_eq!(actual, 7811);
+    }
+
+    #[test]
+    fn test_badge() {
+        let input = [
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+        ];
+        assert_eq!(badge(&input), 'r');
+    }
+
+    #[test]
+    fn test_part2_with_real_data() {
+        let input = util::read_real_data("day3");
+        let actual = solve_part2(&input);
+        assert_eq!(actual, 2487);
     }
 }

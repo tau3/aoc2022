d6b04be80d322086c6a11d65ad0e6f639a55c070
tau3 <smr.oznob@gmail.com>
2022-12-12 23:07:24 +0300

    fixing compilation

--- src/day5.rs
+++ src/day5.rs
@@ -1,92 +1,98 @@
-fn solve(input: Vector<String>) {
+use std::collections::VecDeque;
+
+pub fn solve(input: Vec<&str>) -> String {
     let input = input.into();
-    let (initial, moves) = parse(input);
-    let stacks = parse_initial(initial);
+    let (initial, moves) = parse(&input);
+    let mut stacks = parse_initial(&initial);
 
-    for move_ : moves{
-	let move_ = parse_move(move);
-	apply_move(stacks, move_);
+    for move_ in moves {
+        let move_ = parse_move(&move_);
+        apply_move(&mut stacks, move_);
     }
 
-
+    headers(stacks)
 }
 
-fn headers(stacks: [VecDeque<String>; POSITIONS.size()]){
+fn headers(stacks: [VecDeque<char>; POSITIONS.len()]) -> String {
     let mut result = String::new();
-    for stack in stacks {
-	let item = stack.pop_front();
-	result.push(item);
+    for mut stack in stacks {
+        let item = stack.pop_front().unwrap();
+        result.push(item);
     }
     result
 }
 
-fn parse(mut input: VecDeque<String>)-> (Vec<String>, Vec<String>){
+fn parse<'a>(input: &'a VecDeque<&str>) -> (Vec<&'a str>, Vec<&'a str>) {
     let mut initial = Vec::new();
-    let mut moves = Vec::new();
-
-    let target = &initial;
-
-    while input.pop_front() == Some(item) {
-	if(!item.empty()){
-	    initial.push(item);
-	}
-	else{
-	break;}
+   
+    while let Some(item) = input.pop_front() {
+        if !item.is_empty() {
+            initial.push(item);
+        } else {
+            break;
+        }
     }
 
-    (initial, input)
+    (initial, (*input).into())
 }
 
-const POSITIONS = [1,5,9,13,17,21,25,29,33];
+const POSITIONS: [usize; 9] = [1, 5, 9, 13, 17, 21, 25, 29, 33];
 
-fn parse_initial(initial: &Vec<String>)-> [VecDeque<char>; POSITIONS.size()]{
-    let result = [];
+fn parse_initial(initial: &Vec<&str>) -> [VecDeque<char>; POSITIONS.len()] {
+    let mut result = vec![VecDeque::new(); POSITIONS.len()];
     for line in initial {
-	for i in POSITIONS{
-	    if line[i] != ' '{
-		result[i].push_front(line[i]);
-	    }
-	}
-	
-	let tokens =line.split(" ").collect();
-	// 1, 5, 9, 13, 17
+        for i in POSITIONS {
+            let chars: Vec<char> = line.chars().collect();
+            if chars[i] != ' ' {
+                result[i].push_front(chars[i]);
+            }
+        }
     }
 
-    result
+    result.try_into().unwrap()
 }
 
-fn parse_move(move: &String) -> (u32, u32, u32){
-    let mut tokens  =move.split(" ");
-    let amount = tokens.nth(1);
-    let from = tokens.nth(1);
-    let to = tokens.nth(1);
-    (amount, from, to)
+fn parse_move(move_: &str) -> (u32, u32, u32) {
+    let mut tokens = move_.split(" ");
+    let amount = tokens.nth(1).unwrap();
+    let from = tokens.nth(1).unwrap();
+    let to = tokens.nth(1).unwrap();
+    (
+        amount.parse().unwrap(),
+        from.parse().unwrap(),
+        to.parse().unwrap(),
+    )
 }
 
-fn apply_move(mut stacks: &[VecDeque<char>; POSITIONS.size()], (amount, from, to): (u32,u32,u32)) {
-    let source = stacks[from-1];
-    let target = stacks[to-1];
-    for i in 0..amount{
-	let item = source.pop_front().unwrap();
-	target.push_front(item);
+fn apply_move(
+    stacks: &mut [VecDeque<char>; POSITIONS.len()],
+    (amount, from, to): (u32, u32, u32),
+) {
+    let mut source = &mut stacks[(from - 1) as usize];
+    let mut target = &mut stacks[(to - 1) as usize];
+    for _ in 0..amount {
+        let item = source.pop_front().unwrap();
+        target.push_front(item);
     }
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
-    fn test_solve(){
-	let input = vec![
-"    [D]",    
-	    "[N] [C]",
-	    "[Z] [M] [P]",
-	    " 1   2   3 ",
-	    "",
-	    "move 1 from 2 to 1",
-	    "move 3 from 1 to 3",
-	    "move 2 from 2 to 1",
-	    "move 1 from 1 to 2"];
-	assert_eq!(solve(&input), "CMZ");
+    #[test]
+    fn test_solve() {
+        let input = vec![
+            "    [D]",
+            "[N] [C]",
+            "[Z] [M] [P]",
+            " 1   2   3 ",
+            "",
+            "move 1 from 2 to 1",
+            "move 3 from 1 to 3",
+            "move 2 from 2 to 1",
+            "move 1 from 1 to 2",
+        ];
+        assert_eq!(solve(input), "CMZ");
     }
 }

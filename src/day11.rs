6ad95102ac7b18dee676fb0816e3cf24294a96f6
tau3 <smr.oznob@gmail.com>
2022-12-19 01:21:28 +0300

    solve day11 part1

--- src/day11.rs
+++ src/day11.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::{cell::RefCell, collections::VecDeque, rc::Rc};
 
 struct Monkey {
     items: VecDeque<u32>,
@@ -6,16 +6,17 @@ struct Monkey {
     test_delimiter: u32,
     test_true_target: usize,
     test_false_target: usize,
+    count: u32,
 }
 
 impl Monkey {
     fn parse(input: &Vec<&str>) -> Self {
-        let test_false_target = input[5].split(' ').last().unwrap().parse().unwrap();
-        let test_true_target = input[4].split(' ').last().unwrap().parse().unwrap();
+        let test_false_target = last_u32(input[5]) as usize;
+        let test_true_target = last_u32(input[4]) as usize;
         let operation = Operation::parse(input[2]);
-        let test_delimiter = input[3].split(' ').last().unwrap().parse().unwrap();
+        let test_delimiter = last_u32(input[3]);
 
-        let items = input[1]["  Starting items: ".len()..input.len()]
+        let items = input[1]["  Starting items: ".len()..input[1].len()]
             .split(", ")
             .map(|x| x.parse().unwrap())
             .collect();
@@ -26,12 +27,30 @@ impl Monkey {
             test_delimiter,
             test_true_target,
             test_false_target,
+            count: 0,
         }
     }
 
-    fn update_worry(&self, worry: u32) -> u32 {
+    fn take_item(&mut self, worry: u32) {
+        self.items.push_back(worry);
+    }
+
+    fn update_worry(&mut self, worry: u32) -> u32 {
+        self.count += 1;
         self.operation.apply(worry) / 3
     }
+
+    fn receiver(&self, worry: u32) -> usize {
+        if worry % self.test_delimiter == 0 {
+            self.test_true_target
+        } else {
+            self.test_false_target
+        }
+    }
+}
+
+fn last_u32(line: &str) -> u32 {
+    line.split(' ').last().unwrap().parse().unwrap()
 }
 
 enum Operation {
@@ -48,11 +67,11 @@ impl Operation {
 
         let mut tokens = line.split(' ').rev();
         let val = tokens.next().unwrap().parse().unwrap();
-        let operation = tokens.nth_back(1).unwrap();
+        let operation = tokens.next().unwrap();
         match operation {
             "*" => return Operation::Mul(val),
             "+" => return Operation::Plus(val),
-            _ => panic!("unknown opetaion {}", val),
+            _ => panic!("unknown operaation {}", val),
         }
     }
 
@@ -66,7 +85,7 @@ impl Operation {
 }
 
 struct Monkeys {
-    monkeys: Vec<Monkey>,
+    monkeys: Vec<Rc<RefCell<Monkey>>>,
 }
 
 impl Monkeys {
@@ -76,27 +95,31 @@ impl Monkeys {
         for i in 0..input.len() / 6 {
             let part: Vec<&str> = input[i * 6..i * 6 + 6].into_iter().map(|x| *x).collect();
             let monkey = Monkey::parse(&part);
-            monkeys.push(monkey);
+            monkeys.push(Rc::new(RefCell::new(monkey)));
         }
         Self { monkeys }
     }
 
     fn turn(&mut self) {
-        for mut monkey in self.monkeys.iter_mut() {
-            if let Some(worry) = monkey.items.pop_front() {
+        for monkey in self.monkeys.iter() {
+	    // println!("Monkey _:");
+            let mut monkey = monkey.borrow_mut();
+            while let Some(worry) = monkey.items.pop_front() {
+		// println!("  Monkey inspects an item with a worry level of {}.", worry);
                 let worry = monkey.update_worry(worry);
-                let receiver = if worry % monkey.test_delimiter == 0 {
-                    monkey.test_true_target
-                } else {
-                    monkey.test_false_target
-                };
-                self.monkeys[receiver].items.push_back(worry);
+		// println("    Worry level is multiplied to {}.", worry);
+                let receiver = monkey.receiver(worry);
+		// println!("    Monkeys gets bored with item. Worry level is divided by 3 to {}.", worry);
+		// println!("    Iter with worry level {} is thrown to monkey {}", worry, receiver);
+                self.monkeys[receiver].borrow_mut().take_item(worry);
             }
         }
     }
 
     fn monkey_business(&self) -> u32 {
-        0
+        let mut counts: Vec<u32> = self.monkeys.iter().map(|m| m.borrow().count).collect();
+        counts.sort_by(|a, b| b.cmp(a));
+        counts[0] * counts[1]
     }
 }
 
@@ -112,6 +135,7 @@ pub fn solve(input: &Vec<&str>) -> u32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::util;
 
     #[test]
     fn test_solve() {
@@ -144,6 +168,13 @@ mod tests {
             "    If true: throw to monkey 0",
             "    If false: throw to monkey 1",
         ];
-        assert_eq!(solve(&input), 1123);
+        assert_eq!(solve(&input), 10605);
     }
-}
+
+    #[test]
+    fn test_solve_with_real_data() {
+        let data = util::read_real_data("day11");
+        let data = data.iter().map(|line|line.as_str()).collect();
+        assert_eq!(solve(&data), 54752);
+    }
+}       

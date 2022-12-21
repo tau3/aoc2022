c7fe091ae966d5303b80cba6879ed5c24436ba49
tau3 <smr.oznob@gmail.com>
2022-12-22 00:50:21 +0300

    solve basic case

--- src/day12.rs
+++ src/day12.rs
@@ -1,13 +1,13 @@
-// TODO memo
+use std::collections::{HashMap, HashSet};
 
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+type Point = (usize, usize);
 
 struct Grid {
     grid: Vec<Vec<char>>,
     width: usize,
     height: usize,
-    start: (usize, usize),
-    end: (usize, usize),
+    start: Point,
+    end: Point,
 }
 
 impl Grid {
@@ -24,65 +24,71 @@ impl Grid {
         }
     }
 
+    fn solve(&self) -> u32 {
+        let mut memo = HashMap::new();
+        let mut visited = HashSet::new();
+        self.shortest_path(self.end, &mut memo, &mut visited)
+            .unwrap()
+    }
+
     fn shortest_path(
         &self,
-        current: (usize, usize),
-        prev: (usize, usize),
-        memo: &mut HashMap<(usize, usize), Option<u32>>,
+        current: Point,
+        memo: &mut HashMap<Point, Option<u32>>,
+        visited: &mut HashSet<Point>,
     ) -> Option<u32> {
         if current == self.start {
-            println!("BINGO");
             return Some(0);
         }
-        let mut adjacent = self.adjacent(current);
-        let position = adjacent.iter().position(|x| *x == prev);
-        if let Some(pos) = position {
-            adjacent.remove(pos);
-        }
+        visited.insert(current);
 
-        println!("point {:?}, adjacent {:?}", current, adjacent);
-        let mut cand = Vec::new();
-        // let mut m1 = memo.borrow_mut();
-        for point in adjacent {
+        let mut candidates = Vec::new();
+        for point in self.not_visited_adjacent(visited, current) {
             if !self.can_jump(point, current) {
                 continue;
             }
-            if let Some(dest) = self.get_dest(memo, point, current) {
-                cand.push(dest);
+            if let Some(dest) = self.get_dest(memo, visited, point) {
+                candidates.push(dest);
             }
         }
-        cand.iter().min().map(|x| x + 1)
+        candidates.iter().min().map(|result| result + 1)
+    }
+
+    fn not_visited_adjacent(&self, visited: &HashSet<Point>, current: Point) -> Vec<Point> {
+        let adjacent = self.adjacent(current);
+        adjacent
+            .iter()
+            .filter(|point| !visited.contains(point))
+            .map(|(col, row)| (*col, *row))
+            .collect()
     }
 
     fn get_dest(
         &self,
-        memo: &mut HashMap<(usize, usize), Option<u32>>,
-        point: (usize, usize),
-        current: (usize, usize),
+        memo: &mut HashMap<Point, Option<u32>>,
+        visited: &mut HashSet<Point>,
+        point: Point,
     ) -> Option<u32> {
-        let mut maybe_path = None;
-        // let mut m1 = memo;
-        let maybe_dest = memo.get(&point);
-        if maybe_dest.is_none() {
-            maybe_path = self.shortest_path(point, current, memo);
-            memo.insert(point, maybe_path);
+        let mut result = None;
+        let maybe_path_length = memo.get(&point);
+        if maybe_path_length.is_none() {
+            result = self.shortest_path(point, memo, visited);
+            memo.insert(point, result);
         }
-
-        // m1.drop();
-        maybe_path
+        result
     }
 
-    fn at(&self, (col, row): (usize, usize)) -> char {
+    fn at(&self, (col, row): Point) -> char {
         self.grid[row][col]
     }
 
-    fn can_jump(&self, from: (usize, usize), to: (usize, usize)) -> bool {
+    fn can_jump(&self, from: Point, to: Point) -> bool {
         let from = self.at(from);
         let to = self.at(to);
         can_jump(from, to)
     }
 
-    fn adjacent(&self, (col, row): (usize, usize)) -> Vec<(usize, usize)> {
+    fn adjacent(&self, (col, row): Point) -> Vec<Point> {
         let (col, row) = (col as i32, row as i32);
         let result = vec![
             (col, row + 1),
@@ -93,11 +99,13 @@ impl Grid {
 
         result
             .iter()
-            .filter(|(col, row)| {
-                *col >= 0 && *row >= 0 && *col < self.width as i32 && *row < self.height as i32
-            })
+            .filter(|(col, row)| self.fits_grid((col, row)))
             .map(|(col, row)| (*col as usize, *row as usize))
             .collect()
+    }
+
+    fn fits_grid(&self, (col, row): (&i32, &i32)) -> bool {
+        *col >= 0 && *row >= 0 && *col < self.width as i32 && *row < self.height as i32
     }
 }
 
@@ -106,24 +114,26 @@ fn add1_char(c: char) -> char {
 }
 
 fn can_jump(from: char, to: char) -> bool {
-    if from == 'S' || to == 'E' || from == to {
+    if from == 'S' || from == to {
         return true;
     };
+    if to == 'E' {
+        return from == 'z' || from == 'y';
+    }
 
     add1_char(from) == to
 }
 
 pub fn solve(input: Vec<Vec<char>>) -> u32 {
     let grid = Grid::new(input);
-    let mut x = HashMap::new();
-    grid.shortest_path(grid.end, grid.end, &mut x).unwrap()
+    grid.solve()
 }
 
-fn find_start(input: &Vec<Vec<char>>) -> ((usize, usize), (usize, usize)) {
+fn find_start(input: &[Vec<char>]) -> (Point, Point) {
     let mut start = (0, 0);
     let mut end = (0, 0);
-    for row in 0..input.len() {
-        for col in 0..input[row].len() {
+    for (row, line) in input.iter().enumerate() {
+        for col in 0..line.len() {
             if input[row][col] == 'S' {
                 start = (col, row);
             } else if input[row][col] == 'E' {
@@ -131,8 +141,6 @@ fn find_start(input: &Vec<Vec<char>>) -> ((usize, usize), (usize, usize)) {
             }
         }
     }
-    println!("start {:?}, end {:?}", start, end);
-
     (start, end)
 }
 

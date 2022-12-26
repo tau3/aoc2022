0f5217925781709893fa6d8fd103dcbc2ce5d7fd
tau3 <smr.oznob@gmail.com>
2022-12-26 21:03:10 +0300

    solution for day15pr2

--- src/day15.rs
+++ src/day15.rs
@@ -2,11 +2,51 @@ use std::collections::HashSet;
 
 type Point = (i32, i32);
 
+struct Area {
+    sensor: Point,
+    radius: usize,
+}
+
+impl Area {
+    fn from(sensor: Point, beacon: Point) -> Self {
+        let radius = manhattan_distance(&sensor, &beacon);
+        Self { sensor, radius }
+    }
+
+    fn contains(&self, point: &Point) -> bool {
+        manhattan_distance(&self.sensor, point) <= self.radius
+    }
+}
+
+pub fn part2(input: Vec<&str>, border:usize) -> usize {
+    let input = parse(input);
+    solve_part2(input, border)
+}
+
+fn solve_part2(input: Vec<(Point, Point)>, border: usize) -> usize {
+    let areas: Vec<Area> = input.iter().map(move |(s, b)| Area::from(*s, *b)).collect();
+    for col in 0..=border {
+        for row in 0..=border {
+            let mut found = true;
+            for area in areas.iter() {
+                if area.contains(&(col as i32, row as i32)) {
+                    found = false;
+                    break;
+                }
+            }
+            if found {
+                return col * 4000000 + row;
+            }
+        }
+    }
+    unreachable!("fail")
+}
+
 fn manhattan_distance((x1, y1): &Point, (x2, y2): &Point) -> usize {
     ((x1 - x2).abs() + (y1 - y2).abs()) as usize
 }
 
-fn area((col, row): &Point, radius: usize, target_row: usize) -> Vec<Point> {
+fn covered_area_on_row((col, row): &Point, radius: usize, target_row: usize) -> Vec<Point> {
     let radius_signed = radius as i32;
     let mut smallest = 0;
     if radius_signed <= *row {
@@ -32,16 +72,17 @@ fn solve(input: Vec<(Point, Point)>, row: usize) -> usize {
         taken.insert(sensor);
         taken.insert(beacon);
     }
-    let mut result = HashSet::new();
+    let mut result = Vec::new();
     for (sensor, beacon) in input.iter() {
         let radius = manhattan_distance(sensor, beacon);
-        let area = area(sensor, radius, row);
+        let area = covered_area_on_row(sensor, radius, row);
         for point in area {
             if !taken.contains(&point) {
-                result.insert(point);
+                result.push(point);
             }
         }
     }
+    let result: HashSet<_> = result.iter().collect();
     result.len()
 }
 
@@ -84,6 +125,14 @@ mod tests {
     }
 
     #[test]
+    fn test_part2_with_real_data() {
+        let input = util::read_real_data("day15");
+        let input = input.iter().map(|line| line.as_str()).collect();
+        assert_eq!(part2(input, 4000000), 123);
+    }
+
+    
+    #[test]
     fn test_part1() {
         let input = vec![
             "Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
@@ -104,4 +153,27 @@ mod tests {
 
         assert_eq!(part1(input, 10), 26);
     }
+
+    #[test]
+    fn test_part2() {
+        let input = vec![
+            "Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
+            "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+            "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+            "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+            "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+            "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+            "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+            "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+            "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+            "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+            "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+            "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+            "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+            "Sensor at x=20, y=1: closest beacon is at x=15, y=3",
+        ];
+
+        assert_eq!(part2(input, 20),56000011);
+    }
+
 }

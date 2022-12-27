9726110949d3f04e02ad2f1a4d32429612e6db02
tau3 <smr.oznob@gmail.com>
2022-12-27 21:55:47 +0300

    solution for day20pt1

--- src/day20.rs
+++ src/day20.rs
@@ -1,19 +1,14 @@
 use std::collections::VecDeque;
 
 pub fn solve(mut data: &mut VecDeque<i32>) -> i32 {
-    let mut result = 0;
     let order: Vec<i32> = data.iter().copied().collect();
-    for i in 1..=3005 {
-        let current = order[(i-1) % order.len()];
-	// println!("current {}", current);
-        permute(&mut data, current);
-        if [1000, 2000, 3000].contains(&i) {
-            let index = index_of(&data, 0) + 1;
-	    let kek = data[index];
-	    println!("ith: {}", kek);
-            result += data[index];
-        }
+    for current in order.iter() {
+        permute(&mut data, *current);
     }
+    let zero_index = index_of(&data, 0);
+    let result = data[(zero_index + 1000) % (order.len())]
+        + data[(zero_index + 2000) % (order.len())]
+        + data[(zero_index + 3000) % (order.len())];
     result
 }
 
@@ -24,30 +19,33 @@ fn index_of(data: &VecDeque<i32>, current: i32) -> usize {
 
 fn permute(data: &mut VecDeque<i32>, current: i32) {
     if current == 0 {
-	return;
+        return;
     }
-    
+
     let pos = index_of(data, current);
-    // println!("permute {} on pos {}", current, pos);
     data.remove(pos);
 
     let mut pos = pos as i32 + current;
     if pos < 0 {
-        pos = data.len() as i32 + pos;
+        while pos < 0 {
+            pos = data.len() as i32 + pos;
+        }
     } else if pos > data.len() as i32 {
-        pos = pos - data.len() as i32;
+        while pos > data.len() as i32 {
+            pos = pos - data.len() as i32;
+        }
     }
     let mut pos = pos as usize;
     if pos == 0 {
-	pos = data.len();
+        pos = data.len();
     }
-    // println!("new pos: {}", pos);
     data.insert(pos, current);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::util;
 
     #[test]
     fn test_permute() {
@@ -72,18 +70,29 @@ mod tests {
         let actual: Vec<i32> = data.iter().copied().collect();
         assert_eq!(actual, vec![1, 2, -3, 0, 3, 4, -2]);
 
-     	permute(&mut data, 0);
+        permute(&mut data, 0);
         let actual: Vec<i32> = data.iter().copied().collect();
         assert_eq!(actual, vec![1, 2, -3, 0, 3, 4, -2]);
 
-       	permute(&mut data, 4);
+        permute(&mut data, 4);
         let actual: Vec<i32> = data.iter().copied().collect();
         assert_eq!(actual, vec![1, 2, -3, 4, 0, 3, -2]);
     }
 
     #[test]
     fn test_solve() {
-	let mut data: VecDeque<i32> = VecDeque::from([1, 2, -3, 3, -2, 0, 4]);
-	assert_eq!(solve(&mut data), 3);
+        let mut data: VecDeque<i32> = VecDeque::from([1, 2, -3, 3, -2, 0, 4]);
+        assert_eq!(solve(&mut data), 3);
+    }
+
+    #[test]
+    fn test_with_real_data() {
+        let input = util::read_real_data("day20");
+        let mut input: VecDeque<i32> = input
+            .iter()
+            .map(|line| line.parse())
+            .map(|result| result.unwrap())
+            .collect();
+        assert_eq!(solve(&mut input), 7462);
     }
 }

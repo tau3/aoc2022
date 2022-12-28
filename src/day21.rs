a13f49215b325d8e2f30998ab40ef4f70d1ea49f
tau3 <smr.oznob@gmail.com>
2022-12-28 20:29:35 +0300

    try solve day21pt2

--- src/day21.rs
+++ src/day21.rs
@@ -8,6 +8,12 @@ enum Expr<'a> {
 }
 
 impl<'a> Expr<'a> {
+    fn decompose(&self) -> (&str, &Operation, &str) {
+        match self {
+            Expr::Number(_) => panic!("simple number!"),
+            Expr::Calc(left, oper, right) => (left, &oper, right),
+        }
+    }
     fn eval(&self, registry: &HashMap<&str, Expr>) -> Uint {
         match self {
             Expr::Number(x) => *x,
@@ -30,6 +36,35 @@ impl<'a> Expr<'a> {
         }
         Self::Number(expr.parse().unwrap())
     }
+
+    fn is_depend_on_human(&self, registry: &HashMap<&str, Expr>) -> bool {
+        match self {
+            Expr::Number(_) => false,
+            Expr::Calc(left, _, right) => {
+                if right == &"humn" || left == &"humn" {
+                    return true;
+                }
+                let left = registry.get(left).unwrap();
+                let right = registry.get(right).unwrap();
+                return left.is_depend_on_human(registry) || right.is_depend_on_human(registry);
+            }
+        }
+    }
+
+    fn adjust_human(&self, registry: &HashMap<&str, Expr>, operation_result: Uint) -> Uint {
+        let (left, oper, right) = self.decompose();
+        let left = registry.get(left).unwrap();
+        let right = registry.get(right).unwrap();
+        if left.is_depend_on_human(registry) {
+            let right = right.eval(registry);
+            let operation_result = oper.reverse_left_var(right, operation_result);
+            return left.adjust_human(registry, operation_result);
+        } else {
+            let left = left.eval(registry);
+            let operation_result = oper.reverse_right_var(left, operation_result);
+            return right.adjust_human(registry, operation_result);
+        }
+    }
 }
 
 enum Operation {
@@ -40,6 +75,24 @@ enum Operation {
 }
 
 impl Operation {
+    fn reverse_left_var(&self, right: Uint, operation_result: Uint) -> Uint {
+        match self {
+            Operation::Add => operation_result - right,
+            Operation::Sub => operation_result + right,
+            Operation::Mul => operation_result / right,
+            Operation::Div => operation_result * right,
+        }
+    }
+
+    fn reverse_right_var(&self, left: Uint, operation_result: Uint) -> Uint {
+        match self {
+            Operation::Add => operation_result - left,
+            Operation::Sub => left - operation_result,
+            Operation::Mul => operation_result / left,
+            Operation::Div => left / operation_result,
+        }
+    }
+
     fn new(expr: &str) -> Self {
         match expr {
             "+" => Operation::Add,
@@ -74,6 +127,29 @@ pub fn solve(input: &Vec<&str>) -> Uint {
     root.unwrap().eval(&registry)
 }
 
+pub fn part2(input: &Vec<&str>) -> Uint {
+    let mut registry: HashMap<&str, Expr> = HashMap::new();
+    for line in input {
+        let mut name_and_expr = line.split(": ");
+        let name = name_and_expr.next().unwrap();
+        let expr = name_and_expr.next().unwrap();
+        let expr = Expr::new(expr);
+        registry.insert(name, expr);
+    }
+
+    let root = registry.get("root").unwrap();
+    let (left, _, right) = root.decompose();
+    let left = registry.get(left).unwrap();
+    let right = registry.get(right).unwrap();
+    if left.is_depend_on_human(&registry) {
+        let target = right.eval(&registry);
+        return left.adjust_human(&registry, target);
+    } else {
+        let target = left.eval(&registry);
+        return right.adjust_human(&registry, target);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -99,6 +175,28 @@ mod tests {
             "hmdt: 32",
         ];
         assert_eq!(solve(&input), 152);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = vec![
+            "root: pppw + sjmn",
+            "dbpl: 5",
+            "cczh: sllz + lgvd",
+            "zczc: 2",
+            "ptdq: humn - dvpt",
+            "dvpt: 3",
+            "lfqf: 4",
+            "humn: 5",
+            "ljgn: 2",
+            "sjmn: drzm * dbpl",
+            "sllz: 4",
+            "pppw: cczh / lfqf",
+            "lgvd: ljgn * ptdq",
+            "drzm: hmdt - zczc",
+            "hmdt: 32",
+        ];
+        assert_eq!(part2(&input), 301);
     }
 
     #[test]

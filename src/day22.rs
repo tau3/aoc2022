0306a6b3654d5481381ffed9a3b8f427ffc62c39
tau3 <smr.oznob@gmail.com>
2022-12-29 19:04:10 +0300

    make solution work

--- src/day22.rs
+++ src/day22.rs
@@ -5,34 +5,91 @@ struct Grid {
 }
 
 impl Grid {
-    fn new(raw:&Vec<&str>)->Self {
-	todo!()
-    }
-    
-    fn is_wall(&self, col:usize, row:usize) -> bool {
-	self.grid[row][col] == '#'
+    fn min_wrap_col(&self, col: usize) -> (usize, usize) {
+        let column: Vec<char> = self.col(col);
+        for (i, point) in column.iter().enumerate() {
+            if *point != ' ' {
+                return (col, i);
+            }
+        }
+        unreachable!()
     }
 
-    fn is_out_of_bounds(&self, col:usize, row:usize)->bool{
-	if col < 0 || row < 0 {
-	    return true;
-	}
-
-	if row >= self.grid.len() {
-	    return true;
-	}
-
-	let row = &self.grid[row];
-	if col >= row.len() {
-	    return true;
-	}
-	return row[col] == ' ';
+    fn max_wrap_col(&self, col: usize) -> (usize, usize) {
+        let column: Vec<char> = self.col(col);
+        for (i, point) in column.iter().enumerate() {
+            if *point != ' ' {
+                return (col, i);
+            }
+        }
+        unreachable!()
     }
-    
+
+    fn max_wrap_row(&self, row: usize) -> (usize, usize) {
+        let full_row = &self.grid[row];
+        for i in full_row.len() - 1..=0 {
+            if full_row[i] != ' ' {
+                return (i, row);
+            }
+        }
+        unreachable!()
+    }
+
+    fn min_wrap_row(&self, row: usize) -> (usize, usize) {
+        let full_row = &self.grid[row];
+        for (i, point) in full_row.iter().enumerate() {
+            if *point != ' ' {
+                return (i, row);
+            }
+        }
+        unreachable!()
+    }
+
+    fn col(&self, col: usize) -> Vec<char> {
+        let mut result = Vec::new();
+        for line in self.grid.iter() {
+            if col >= line.len() {
+                result.push(' ');
+            } else {
+                result.push(line[col]);
+            }
+        }
+        result
+    }
+
+    fn new(raw: &[&str]) -> Self {
+        let grid = raw
+            .iter()
+            .map(|line| line.chars())
+            .map(|chars| chars.collect())
+            .collect();
+        Self { grid }
+    }
+
+    fn is_wall(&self, col: usize, row: usize) -> bool {
+        self.grid[row][col] == '#'
+    }
+
+    fn is_out_of_bounds(&self, col: usize, row: usize) -> bool {
+        // if col < 0 || row < 0 {
+        // return true;
+        // }
+
+        if row >= self.grid.len() {
+            return true;
+        }
+
+        let row = &self.grid[row];
+        if col >= row.len() {
+            return true;
+        }
+        row[col] == ' '
+    }
+
     fn find_start(&self) -> usize {
         let topmost = &self.grid[0];
-        for i in 0..topmost.len() {
-            if topmost[i] == '.' {
+        for (i, point) in topmost.iter().enumerate() {
+            if *point == '.' {
                 return i;
             }
         }
@@ -50,11 +107,11 @@ enum Direction {
 
 impl From<char> for Direction {
     fn from(c: char) -> Self {
-	match c {
-	    'R' => Direction::Right,
-	    'L' => Direction::Left,
-	    _ => panic!("{} direction is not supported", c)
-	}
+        match c {
+            'R' => Direction::Right,
+            'L' => Direction::Left,
+            _ => panic!("{} direction is not supported", c),
+        }
     }
 }
 
@@ -79,6 +136,7 @@ enum Action {
     Turn(Direction),
 }
 
+#[derive(Debug)]
 struct Cursor {
     col: usize,
     row: usize,
@@ -87,15 +145,19 @@ struct Cursor {
 
 impl Cursor {
     fn position(&self) -> (usize, usize) {
-	(self.col, self.row)
+        (self.col, self.row)
     }
-    
-    fn turn(&self, to:&Direction) -> Self {
-	Self{col:self.col, row:self.row, direction:self.direction.turn(to)}
+
+    fn turn(&self, to: &Direction) -> Self {
+        Self {
+            col: self.col,
+            row: self.row,
+            direction: self.direction.turn(to),
+        }
     }
-    
+
     fn step(&self) -> Self {
-        return match self.direction {
+        match self.direction {
             Direction::Up => Self {
                 col: self.col,
                 row: self.row - 1,
@@ -116,7 +178,7 @@ impl Cursor {
                 row: self.row,
                 direction: self.direction,
             },
-        };
+        }
     }
 
     fn evaluate(&self) -> usize {
@@ -126,7 +188,7 @@ impl Cursor {
             Direction::Left => 2,
             Direction::Up => 3,
         };
-        return (self.row + 1) * 1000 + (self.col + 1) * 4 + score;
+        (self.row + 1) * 1000 + (self.col + 1) * 4 + score
     }
 }
 
@@ -136,33 +198,55 @@ struct Trip {
 }
 
 impl Trip {
-    fn new(input:&Vec<&str>) -> Self {
-	let grid = Grid::new(input);
+    fn new(input: &[&str]) -> Self {
+        let grid = Grid::new(input);
         let cursor = Cursor {
             col: grid.find_start(),
-            row: 1,
+            row: 0,
             direction: Direction::Right,
         };
         Self { grid, cursor }
     }
 
     fn action(&mut self, action: &Action) {
+        println!("before move: {:?}", self.cursor);
         match action {
             Action::Turn(to) => self.cursor = self.cursor.turn(to),
             Action::Go(distance) => {
                 for _ in 0..*distance {
-                    let updated  = self.cursor.step();
-		    let (col, row) = updated.position();
+                    let updated = self.cursor.step();
+                    let (col, row) = updated.position();
                     if self.grid.is_out_of_bounds(col, row) {
-                        todo!();
+                        if let Some((c, r)) = self.wrap() {
+                            self.cursor = Cursor {
+                                col: c,
+                                row: r,
+                                direction: self.cursor.direction,
+                            };
+                            break;
+                        }
                     }
-                    if self.grid.is_wall(col, row){
+                    if self.grid.is_wall(col, row) {
                         break;
                     }
                     self.cursor = updated;
                 }
             }
         }
+    }
+
+    fn wrap(&mut self) -> Option<(usize, usize)> {
+        let target @ (col, row) = match self.cursor.direction {
+            Direction::Down => self.grid.min_wrap_col(self.cursor.col),
+            Direction::Up => self.grid.max_wrap_col(self.cursor.col),
+            Direction::Left => self.grid.max_wrap_row(self.cursor.row),
+            Direction::Right => self.grid.min_wrap_row(self.cursor.row),
+        };
+
+        if self.grid.is_wall(col, row) {
+            return None;
+        }
+        Some(target)
     }
 
     fn password(&self) -> usize {
@@ -176,7 +260,7 @@ struct Commands {
 
 impl Commands {
     fn new(input: &Vec<&str>) -> Self {
-	let raw = input[input.len()-1];
+        let raw = input[input.len() - 1];
         let state = raw.chars().collect();
         Self { state }
     }
@@ -188,17 +272,25 @@ impl Commands {
             Some(x) => {
                 let mut res = String::from(x);
                 while let Some(y) = self.state.pop_front() {
-                    res.push(y);
+                    if y.is_numeric() {
+                        res.push(y);
+                    } else {
+                        self.state.push_front(y);
+                        break;
+                    }
                 }
-                Some(Action::Go(res.parse().unwrap()))
+                Some(Action::Go(
+                    res.parse()
+                        .unwrap_or_else(|_| panic!("can't parse {}", res)),
+                ))
             }
         }
     }
 }
 
 pub fn solve(input: &Vec<&str>) -> usize {
-    let mut trip = Trip::new(&input);
-    let mut  commands = Commands::new(&input);
+    let mut trip = Trip::new(input);
+    let mut commands = Commands::new(input);
 
     while let Some(command) = commands.pop_command() {
         trip.action(&command);

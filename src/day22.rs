8dc972be856e00cc2cac98227f95685463248ead
tau3 <smr.oznob@gmail.com>
2022-12-29 18:08:37 +0300

    fixing compilation

--- src/day22.rs
+++ src/day22.rs
@@ -1,36 +1,76 @@
+use std::collections::VecDeque;
+
 struct Grid {
-    grid: Vec<Vec<char>>
+    grid: Vec<Vec<char>>,
 }
 
 impl Grid {
-    fn find_start(&self) -> usize {
-	let topmost = grid[0];
-	for i in 0..topmost.len() {
-	    if grid[i] == '.' {
-		return i;
-	    }
+    fn new(raw:&Vec<&str>)->Self {
+	todo!()
+    }
+    
+    fn is_wall(&self, col:usize, row:usize) -> bool {
+	self.grid[row][col] == '#'
+    }
+
+    fn is_out_of_bounds(&self, col:usize, row:usize)->bool{
+	if col < 0 || row < 0 {
+	    return true;
 	}
-	panic!()
+
+	if row >= self.grid.len() {
+	    return true;
+	}
+
+	let row = &self.grid[row];
+	if col >= row.len() {
+	    return true;
+	}
+	return row[col] == ' ';
+    }
+    
+    fn find_start(&self) -> usize {
+        let topmost = &self.grid[0];
+        for i in 0..topmost.len() {
+            if topmost[i] == '.' {
+                return i;
+            }
+        }
+        panic!()
     }
 }
 
+#[derive(Debug, Clone, Copy)]
 enum Direction {
-    Up, Down, Left, Right
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl From<char> for Direction {
+    fn from(c: char) -> Self {
+	match c {
+	    'R' => Direction::Right,
+	    'L' => Direction::Left,
+	    _ => panic!("{} direction is not supported", c)
+	}
+    }
 }
 
 impl Direction {
-    fn turn(&self, to: Direction) -> Self {
-	match (self, to) {
-	    (Up, Left) => Left,
-	    (Up, Right) => Right,
-	    (Down, Left) => Right,
-	    (Down, Right) => Left,
-	    (Left, Left) => Down,
-	    (Left, Right) => Up,
-	    (Right, Left) => Up,
-	    (Right, Right) => Down,
-	    (_, _) => panic!("turn from {} to {} is not supported", self, to)
-	}
+    fn turn(&self, to: &Direction) -> Self {
+        match (self, to) {
+            (Direction::Up, Direction::Left) => Direction::Left,
+            (Direction::Up, Direction::Right) => Direction::Right,
+            (Direction::Down, Direction::Left) => Direction::Right,
+            (Direction::Down, Direction::Right) => Direction::Left,
+            (Direction::Left, Direction::Left) => Direction::Down,
+            (Direction::Left, Direction::Right) => Direction::Up,
+            (Direction::Right, Direction::Left) => Direction::Up,
+            (Direction::Right, Direction::Right) => Direction::Down,
+            (_, _) => panic!("turn from {:?} to {:?} is not supported", self, to),
+        }
     }
 }
 
@@ -46,60 +86,87 @@ struct Cursor {
 }
 
 impl Cursor {
+    fn position(&self) -> (usize, usize) {
+	(self.col, self.row)
+    }
+    
+    fn turn(&self, to:&Direction) -> Self {
+	Self{col:self.col, row:self.row, direction:self.direction.turn(to)}
+    }
+    
     fn step(&self) -> Self {
-	match direction {
-	    Direction::Up => (self.col, self.row-1, self.direction),
-	    Direction::Down => {self.col, self.row+1, self.direction},
-	    Direction::Left => {self.col-1, self.row, self.direction},
-	    Direction::Right => {self.col+1, self.row, self.direction},
-	}
+        return match self.direction {
+            Direction::Up => Self {
+                col: self.col,
+                row: self.row - 1,
+                direction: self.direction,
+            },
+            Direction::Down => Self {
+                col: self.col,
+                row: self.row + 1,
+                direction: self.direction,
+            },
+            Direction::Left => Self {
+                col: self.col - 1,
+                row: self.row,
+                direction: self.direction,
+            },
+            Direction::Right => Self {
+                col: self.col + 1,
+                row: self.row,
+                direction: self.direction,
+            },
+        };
     }
 
     fn evaluate(&self) -> usize {
-	let score = match self.direction {
-	    Direction::Right => 0,
-	    Direction::Down => 1,
-	    Direction::Left => 2,
-	    Direction:: Up => 3,
-	}
-	(self.row +1 )*1000 + (self.col+1)*4 +score
+        let score = match self.direction {
+            Direction::Right => 0,
+            Direction::Down => 1,
+            Direction::Left => 2,
+            Direction::Up => 3,
+        };
+        return (self.row + 1) * 1000 + (self.col + 1) * 4 + score;
     }
 }
 
-struct Trip() {
+struct Trip {
     grid: Grid,
-    cursor: (usize, usize, Direction),
+    cursor: Cursor,
 }
 
 impl Trip {
-    fn new(grid: Grid) -> Self {
-	let cursor = (grid.find_start(), 1, Direction::Right);
-	Self { grid, cursor }
+    fn new(input:&Vec<&str>) -> Self {
+	let grid = Grid::new(input);
+        let cursor = Cursor {
+            col: grid.find_start(),
+            row: 1,
+            direction: Direction::Right,
+        };
+        Self { grid, cursor }
     }
 
     fn action(&mut self, action: &Action) {
-	match action {
-	    Action::Turn(to) => {
-		let (col, row, direction) = self.cursor;
-		self.cursor = (col, row, direction.turn(to))
-	    },
-	    Action::Move(distance) => {
-		for i in 0..distance {
-		    let updated@(col, row, _) = self.cursor.step();
-		    if self.grid.is_out_of_bounds(col, row) || self.grid.is_void(col,row) {
-			todo!();
-		    }
-		    if self.grid.is_wall(col, row) {
-			break;
-		    }
-		    self.cursor = udpated;
-		}
-	    }
-	}
+        match action {
+            Action::Turn(to) => self.cursor = self.cursor.turn(to),
+            Action::Go(distance) => {
+                for _ in 0..*distance {
+                    let updated  = self.cursor.step();
+		    let (col, row) = updated.position();
+                    if self.grid.is_out_of_bounds(col, row) {
+                        todo!();
+                    }
+                    if self.grid.is_wall(col, row){
+                        break;
+                    }
+                    self.cursor = updated;
+                }
+            }
+        }
     }
 
     fn password(&self) -> usize {
-	self.cursor.evaluate()
+        self.cursor.evaluate()
     }
 }
 
@@ -108,62 +175,60 @@ struct Commands {
 }
 
 impl Commands {
-    fn new(raw: String) -> Self {
-	let state = raw.chars().collect();
-	Self { state }
+    fn new(input: &Vec<&str>) -> Self {
+	let raw = input[input.len()-1];
+        let state = raw.chars().collect();
+        Self { state }
     }
 
     fn pop_command(&mut self) -> Option<Action> {
-	match state.pop_front() {
-	    None => None,
-	    Some(x) && x.is_alpha() => { Some(Action::Turn(x.parse().unwrap())) },
-	    Some(x) => {
-		let mut res = String::from(x);
-		while let Some(y) = self.state.pop_front() {
-		    res.push(y);
-		}
-		Some(Action::Move(res.parse().unwrap()))
-	    }
-	}
-	
+        match self.state.pop_front() {
+            None => None,
+            Some(x) if x.is_alphabetic() => Some(Action::Turn(x.into())),
+            Some(x) => {
+                let mut res = String::from(x);
+                while let Some(y) = self.state.pop_front() {
+                    res.push(y);
+                }
+                Some(Action::Go(res.parse().unwrap()))
+            }
+        }
     }
 }
 
 pub fn solve(input: &Vec<&str>) -> usize {
-    let trip = Trip::new(&input);
-    let commads = Commands::new(&input);
+    let mut trip = Trip::new(&input);
+    let mut  commands = Commands::new(&input);
 
     while let Some(command) = commands.pop_command() {
-	trip.action(command);
+        trip.action(&command);
     }
 
     trip.password()
 }
 
-
 #[cfg(test)]
 mod tests {
-    use super:*;
+    use super::*;
 
     #[test]
-    fn test_solve(){
-	let input = vec![
-"        ...#",
-"        .#..",
-"        #...",
-"        ....",
-"...#.......#",
-"........#...",
-"..#....#....",
-"..........#.",
-"        ...#....",
-"        .....#..",
-"        .#......",
-"        ......#.",
-"",
-"10R5L5R10L4R5L5"
-	];
-	assert_eq!(solve(input), 6032);
+    fn test_solve() {
+        let input = vec![
+            "        ...#",
+            "        .#..",
+            "        #...",
+            "        ....",
+            "...#.......#",
+            "........#...",
+            "..#....#....",
+            "..........#.",
+            "        ...#....",
+            "        .....#..",
+            "        .#......",
+            "        ......#.",
+            "",
+            "10R5L5R10L4R5L5",
+        ];
+        assert_eq!(solve(&input), 6032);
     }
 }
-

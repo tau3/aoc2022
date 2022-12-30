f2253da1b126f47f53af947f2703fcbe2eae6ba9
tau3 <smr.oznob@gmail.com>
2022-12-30 22:59:50 +0300

    fix compilation

--- src/day23.rs
+++ src/day23.rs
@@ -5,16 +5,16 @@ use std::{
 
 type Position = (i32, i32);
 
-fn adjacent((col, row): (i32, i32)) -> HashSet<Position> {
+fn adjacent((col, row): &(i32, i32)) -> HashSet<Position> {
     HashSet::from([
-        (col - 1, row - 1),
-        (col, row - 1),
-        (col + 1, row - 1),
-        (col + 1, row),
-        (col + 1, row + 1),
-        (col, row + 1),
-        (col - 1, row + 1),
-        (col - 1, row),
+        (*col - 1, *row - 1),
+        (*col, *row - 1),
+        (*col + 1, *row - 1),
+        (*col + 1, *row),
+        (*col + 1, *row + 1),
+        (*col, *row + 1),
+        (*col - 1, *row + 1),
+        (*col - 1, *row),
     ])
 }
 
@@ -60,7 +60,6 @@ impl State {
         let mut elves = HashSet::new();
         for row_index in 0..input.len() {
             let row = input[row_index];
-            let len = row.len();
             for (col, val) in row.chars().enumerate() {
                 if val == '#' {
                     elves.insert((col as i32, row_index as i32));
@@ -76,39 +75,54 @@ impl State {
         Self { elves, directions }
     }
 
-    fn step(&mut self) {
-        let moving_elves = HashSet::new();
-        for elf in self.elves {
+    fn get_moving_elves(&self) -> HashSet<Position> {
+        let mut moving_elves = HashSet::new();
+        for elf @ (col, row) in self.elves.iter() {
             let adjacent = adjacent(elf);
-            let occupied = adjacent.intersection(&self.elves);
+            let mut occupied = adjacent.intersection(&self.elves);
             if !occupied.next().is_some() {
-                moving_elves.insert(elf);
+                moving_elves.insert((*col, *row));
             }
         }
+        moving_elves
+    }
 
-        let pos_to_elves = HashMap::new();
+    fn step(&mut self) {
+        let moving_elves = self.get_moving_elves();
+
+        let mut pos_to_elves = HashMap::new();
         for elf in moving_elves {
-            for direction in self.directions {
+            for direction in self.directions.iter() {
                 let check_points = direction.check_points(&elf);
                 if !check_points.intersection(&self.elves).next().is_some() {
                     continue;
                 }
                 let new_pos = direction.go(&elf);
-                let xs = pos_to_elves.put_if_absent(new_pos, Vec::new());
-                xs.push(elf);
+                match pos_to_elves.get_mut(&new_pos) {
+                    None => {
+                        pos_to_elves.insert(new_pos, Vec::from([elf]));
+                    }
+                    Some(xs) => {
+                        xs.push(elf);
+                    }
+                };
             }
         }
 
-        for (k, xs) in pos_to_elves {
+        for (k, xs) in pos_to_elves.iter() {
             if xs.len() > 1 {
                 continue;
             }
 
             let new_pos = xs[0];
-            self.elves.remove(k);
+            self.elves.remove(&k);
             self.elves.insert(new_pos);
         }
 
+        self.swap_direction();
+    }
+
+    fn swap_direction(&mut self) {
         let direction = self.directions.pop_front().unwrap();
         self.directions.push_back(direction);
     }
@@ -119,11 +133,11 @@ impl State {
         let mut e = i32::MIN;
         let mut s = i32::MIN;
 
-        for (col, row) in self.elves {
-            n = min(n, row);
-            w = min(w, col);
-            e = max(e, col);
-            s = max(s, row);
+        for (col, row) in self.elves.iter() {
+            n = min(n, *row);
+            w = min(w, *col);
+            e = max(e, *col);
+            s = max(s, *row);
         }
 
         (s - n) * (e - w) - self.elves.len() as i32
@@ -131,8 +145,8 @@ impl State {
 }
 
 pub fn solve(input: &[&str]) -> i32 {
-    let state = State::new(input);
-    for i in 0..10 {
+    let mut state = State::new(input);
+    for _ in 0..10 {
         state.step();
     }
     state.count_empty_ground()

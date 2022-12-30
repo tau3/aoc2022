11aa8fd4c825b7403641db278ba80d2971b98b14
tau3 <smr.oznob@gmail.com>
2022-12-30 19:51:42 +0300

    fixing compilation

--- src/day23.rs
+++ src/day23.rs
@@ -1,120 +1,152 @@
+use std::{
+    cmp::{max, min},
+    collections::{HashMap, HashSet, VecDeque},
+};
+
 type Position = (i32, i32);
 
-fn adjacent((col, row): (i32, i32)) -> [Position; 8] {
-    [
-	(col-1, row-1),
-	(col, row-1),
-	(col+1, row-1),
-	(col+1, row),
-	(col+1, row+1),
-	(col, row+1),
-	(col-1, row+1),
-	(col-1, row)
-    ]
+fn adjacent((col, row): (i32, i32)) -> HashSet<Position> {
+    HashSet::from([
+        (col - 1, row - 1),
+        (col, row - 1),
+        (col + 1, row - 1),
+        (col + 1, row),
+        (col + 1, row + 1),
+        (col, row + 1),
+        (col - 1, row + 1),
+        (col - 1, row),
+    ])
 }
 
-enum Directions {
-    North, East, South, West
+enum Direction {
+    North,
+    East,
+    South,
+    West,
 }
 
-impl Directions{
-    fn check_points(&self, (col,row): &Position) -> [Position; 3]{
-	match self {
-	    Directions::North => [(col-1, row-1, (col, row-1), (col+1, row -1) )],
-	    Directions::East => [(col+1, row-1), (col+1, row-1),( col+1, row +1 )],
-	    Directions::South => [(col-1, row+1),( col, row+1),( col-1, row +1 )],
-	    Directions::West => [(col-1, row-1), (col-1, row), (col-1, row +1 )],
-	}
+impl Direction {
+    fn go(&self, (col, row): &Position) -> Position {
+        match self {
+            Direction::North => (*col, *row - 1),
+            Direction::East => (*col + 1, *row),
+            Direction::South => (*col, *row + 1),
+            Direction::West => (*col - 1, *row),
+        }
+    }
+
+    fn check_points(&self, (col, row): &Position) -> HashSet<Position> {
+        let result = match self {
+            Direction::North => [(*col - 1, *row - 1), (*col, *row - 1), (*col + 1, *row - 1)],
+            Direction::East => [
+                (*col + 1, *row - 1),
+                (*col + 1, *row - 1),
+                (col + 1, row + 1),
+            ],
+            Direction::South => [(*col - 1, *row + 1), (*col, *row + 1), (*col - 1, *row + 1)],
+            Direction::West => [(*col - 1, *row - 1), (*col - 1, *row), (*col - 1, *row + 1)],
+        };
+        HashSet::from(result)
     }
 }
-    
-struct State {
-    elves: Vec<Position>,
-    directions: VecDeque<Direction>
-}
 
+struct State {
+    elves: HashSet<Position>,
+    directions: VecDeque<Direction>,
+}
 
 impl State {
-    fn step(&mut self) {
-	let moving_elves = HashSet::new();
-	for elf in elves {
-	    let adjacet = adjacent(elf);
-	    let occupied = adjacent.intersection(elves);
-	    if !occupied.is_empty() {
-		moving_elves.insert(elf);
-	    }
-	}
-
-	let pos_to_elves = HashMap::new();
-	for elf in moving_elves {
-	    for direction in directions {
-		let check_points = direction.check_points(elf);
-		if !check_points.intersection(elves).is_empty(){
-		    continue;
-		}
-		let new_pos = elf.go_to(direction);
-		let xs = pos_to_elves.put_if_absent(new_pos, Vec::new());
-		xs.push(elf);
-	    }
-	}
-	
-
-	for(k,xs) in pos_to_elves {
-	    if xs.len() > 1 {
-		continue;
-	    }
-
-	    let new_pos = xs[0];
-	    elves.remove(k);
-	    elves.insert(new_pos);
-	}
-
-	let direction = self.directions.pop_front();
-	self.directions.push_back(direction);
+    fn new(input: &[&str]) -> Self {
+        let mut elves = HashSet::new();
+        for row_index in 0..input.len() {
+            let row = input[row_index];
+            let len = row.len();
+            for (col, val) in row.chars().enumerate() {
+                if val == '#' {
+                    elves.insert((col as i32, row_index as i32));
+                }
+            }
+        }
+        let directions = VecDeque::from([
+            Direction::North,
+            Direction::South,
+            Direction::West,
+            Direction::East,
+        ]);
+        Self { elves, directions }
     }
 
-    fn count_empty_ground(&self) -> u32 {
-	let mut n = u32::MAX_VALUE;
-	let mut w = u32::MAX_VALUE;
-	let mut e = u32::MIN_VALUE;
-	let mut s = u32::MIN_VALUE;
-	
-	for (col, row) in self.elves {
-	    n = min(n, row);
-	    w = min(w, col);
-	    e = max(e, col);
-	    s = max(s, row);
-	}
+    fn step(&mut self) {
+        let moving_elves = HashSet::new();
+        for elf in self.elves {
+            let adjacent = adjacent(elf);
+            let occupied = adjacent.intersection(&self.elves);
+            if !occupied.next().is_some() {
+                moving_elves.insert(elf);
+            }
+        }
 
-	(s-n)*(e-w) - self.elves.size()
+        let pos_to_elves = HashMap::new();
+        for elf in moving_elves {
+            for direction in self.directions {
+                let check_points = direction.check_points(&elf);
+                if !check_points.intersection(&self.elves).next().is_some() {
+                    continue;
+                }
+                let new_pos = direction.go(&elf);
+                let xs = pos_to_elves.put_if_absent(new_pos, Vec::new());
+                xs.push(elf);
+            }
+        }
+
+        for (k, xs) in pos_to_elves {
+            if xs.len() > 1 {
+                continue;
+            }
+
+            let new_pos = xs[0];
+            self.elves.remove(k);
+            self.elves.insert(new_pos);
+        }
+
+        let direction = self.directions.pop_front().unwrap();
+        self.directions.push_back(direction);
+    }
+
+    fn count_empty_ground(&self) -> i32 {
+        let mut n = i32::MAX;
+        let mut w = i32::MAX;
+        let mut e = i32::MIN;
+        let mut s = i32::MIN;
+
+        for (col, row) in self.elves {
+            n = min(n, row);
+            w = min(w, col);
+            e = max(e, col);
+            s = max(s, row);
+        }
+
+        (s - n) * (e - w) - self.elves.len() as i32
     }
 }
 
-pub fn solve(input: &Vec<&str>) -> u32 {
+pub fn solve(input: &[&str]) -> i32 {
     let state = State::new(input);
     for i in 0..10 {
-	state.step();
+        state.step();
     }
     state.count_empty_ground()
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
-    fn test_solve(){
-	let input = [
-"....#..",
-"..###.#",
-"#...#.#",
-".#...##",
-"#.###..",
-"##.#.##",
-".#..#..",
-	];
-	assert_eq!(solve(input), 110);
+    fn test_solve() {
+        let input = [
+            "....#..", "..###.#", "#...#.#", ".#...##", "#.###..", "##.#.##", ".#..#..",
+        ];
+        assert_eq!(solve(&input), 110);
     }
-    
 }

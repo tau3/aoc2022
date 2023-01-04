4acb81ff7efeb19f3ba00554f1c3260afbe5fb29
tau3 <smr.oznob@gmail.com>
2023-01-04 22:39:08 +0300

    speed up 19

--- src/day19.rs
+++ src/day19.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, HashMap};
+use std::collections::HashSet;
 
 // const ORE: usize = 0;
 // const CLAY: usize = 1;
@@ -19,23 +19,23 @@ impl State {
     }
 
     fn advance(&self, blueprint: &Blueprint) -> HashSet<State> {
-        let mut result = HashSet::new();
-
-	if let Some(x) = self.build_robot(blueprint, GEODE) {
-	    return HashSet::from([x]);
-	}
-        result.insert(self.just_harvest());
-        for i in 0..self.resources.len()-1 {
-            let variant= self.build_robot(blueprint, i);
-	    if let Some(variant) = variant {
-		result.insert(variant);
-	    }
+        if let Some(state) = self.build_robot(blueprint, GEODE) {
+            return HashSet::from([state]);
         }
-       result 
+
+        let mut result = HashSet::new();
+        result.insert(self.just_harvest());
+        for i in 0..self.resources.len() - 1 {
+            let variant = self.build_robot(blueprint, i);
+            if let Some(variant) = variant {
+                result.insert(variant);
+            }
+        }
+        result
     }
 
     fn just_harvest(&self) -> State {
-        let mut resources = self.resources.clone();
+        let mut resources = self.resources;
         for (i, val) in self.robots.iter().enumerate() {
             resources[i] += val;
         }
@@ -47,7 +47,7 @@ impl State {
 
     fn build_robot(&self, blueprint: &Blueprint, kind: usize) -> Option<State> {
         let prices = blueprint.prices[kind];
-        let mut resources = self.resources.clone();
+        let mut resources = self.resources;
         for i in 0..resources.len() {
             resources[i] -= prices[i];
             if resources[i] < 0 {
@@ -55,36 +55,41 @@ impl State {
             }
             resources[i] += self.robots[i];
         }
-        let mut robots = self.robots.clone();
+        let mut robots = self.robots;
         robots[kind] += 1;
         Some(State { resources, robots })
     }
 }
 
-fn estimate(blueprint: &Blueprint) -> i32 {
+pub fn estimate(blueprint: &Blueprint) -> i32 {
     let initial = State::initial();
     let mut states = vec![HashSet::from([initial])];
-    let mut cache = HashMap::new();
-    for i in 0..=24 {
+    let mut result = 0;
+    for t in 0..=24 {
         let mut next = HashSet::new();
-        for state in states[i].iter() {
-	    if !cache.contains_key(state) {
-		cache.insert(state.clone(), state.advance(blueprint));
-	    }
-            let next_states = &cache[state];
+        for state in states[t].iter() {
+            let next_states = state.advance(blueprint);
             next.extend(next_states);
         }
+        let max_geode = next
+            .iter()
+            .map(|state| state.resources[GEODE])
+            .max()
+            .unwrap();
+	result = result.max(max_geode);
+        next.retain(|state| state.resources[GEODE] == result);
+        // next.retain(|state| state.resources[GEODE] == max_geode);
         states.push(next);
-	println!("size: {} {}", i, states.last().unwrap().len());
+        println!("size: {} {}", t, states.last().unwrap().len());
     }
-    let mut result = 0;
-    for state in states.last().unwrap() {
-        result = result.max(state.resources[GEODE]);
-    }
+    // let mut result = 0;
+    // for state in states.last().unwrap() {
+        // result = result.max(state.resources[GEODE]);
+    // }
     result
 }
 
-struct Blueprint {
+pub struct Blueprint {
     prices: [[i32; 4]; 4],
 }
 
